5430568f256dabf2ef847a97bc400fdd68a11fb0
raythurman2386 <raymondthurman5@gmail.com>
2023-07-18 16:21:02 -0500

    hero and initial hero details complete

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+  selectedHero?: Hero;
+  heroes: Hero[] = []
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
