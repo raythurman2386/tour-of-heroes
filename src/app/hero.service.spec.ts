b5e32450afd59439ea5eadb0287c76ea8f6bb228
raythurman2386 <raymondthurman5@gmail.com>
2023-07-19 16:14:47 -0500

    additional hero service tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-
+import { of } from 'rxjs';
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { Hero } from './Hero';
@@ -9,6 +9,7 @@ import { Hero } from './Hero';
 describe('HeroService', () => {
   let heroService: HeroService;
   let httpTestingController: HttpTestingController;
+  let messageService: MessageService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -39,5 +40,59 @@ describe('HeroService', () => {
 
     req.flush(mockHeroes)
   });
+
+  it('should return a hero when a valid ID is provided', fakeAsync(() => {
+    const mockHero: Hero = { id: 1, name: 'Test Hero' };
+
+    heroService.getHero(1).subscribe((hero) => {
+      expect(hero).toEqual(mockHero);
+    });
+
+    const req = httpTestingController.expectOne('api/heroes/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockHero);
+
+    tick();
+  }));
+
+  it('should log a message when a hero is fetched', fakeAsync(() => {
+    const mockHero: Hero = { id: 1, name: 'Test Hero' };
+
+    heroService.getHero(1).subscribe();
+
+    const req = httpTestingController.expectOne('api/heroes/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockHero);
+
+    tick();
+  }));
+
+  it('should update a hero', fakeAsync(() => {
+    const mockHero: Hero = { id: 1, name: 'Test Hero' };
+
+    heroService.updateHero(mockHero).subscribe();
+
+    const req = httpTestingController.expectOne('api/heroes');
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toBe(mockHero);
+    req.flush({});
+
+    tick();
+  }));
+
+  it('should add a hero', fakeAsync(() => {
+    const mockHero: Hero = { id: 1, name: 'Test Hero' };
+
+    heroService.addHero(mockHero).subscribe((newHero) => {
+      expect(newHero).toEqual(mockHero);
+    });
+
+    const req = httpTestingController.expectOne('api/heroes');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(mockHero);
+    req.flush(mockHero);
+
+    tick();
+  }));
 
 });
